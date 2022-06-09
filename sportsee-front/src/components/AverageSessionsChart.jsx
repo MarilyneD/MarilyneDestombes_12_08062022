956e441bce6adfefe73e2b4c5a8bc0b83d3fddbf
MarilyneD <marilynedestombes@gmail.com>
2022-06-09 21:14:47 +0200

    first chart

---
+++ sportsee-front/src/components/AverageSessionsChart.jsx
@@ -0,0 +1,39 @@
+import React, { PureComponent } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const AverageSessionsChart = ({averagesessions}) => {
+
+
+
+
+    return (
+       
+        <ResponsiveContainer className=
+        "sessions" width="100%" aspect={3}>
+        <LineChart
+          width={500}
+          height={300}
+          data={averagesessions}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="day" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="sessionLength" stroke="#8884d8" activeDot={{ r: 8 }} />
+          </LineChart>
+      </ResponsiveContainer>
+
+
+
+
+    );
+};
+
+export default AverageSessionsChart;
--- sportsee-front/src/pages/User.jsx
+++ sportsee-front/src/pages/User.jsx
@@ -1,45 +1,73 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import Duration from "../components/Duration";
+import ActivityChart from "../components/ActivityChart";
+import AverageSessionsChart from "../components/AverageSessionsChart";
+
+
 
 const User = () => {
     let { id } = useParams();
     //let navigate = useNavigate();
-   //console.log("id",id);
-
-    const [data, setData] = useState([]);
-    useEffect(() => {
-      axios.get("http://localhost:3000/user/"+id).then((res) => setData(res.data));
-    }, []);
-  
-    console.log("data", data.data);
-
-
+   
+    const [mainData, setMainData] = useState([]);
+    const [activity, setActivity] = useState([]);
     const [averagesessions, setAvergagesessions] = useState([]);
-    useEffect(() => {
-      axios.get("http://localhost:3000/user/"+id+"/average-sessions").then((res) => {setAvergagesessions(res.data);});
-    }, []);
-  
-    console.log("averagesessions.data", averagesessions.data);
-
-    console.log("sessions sessions", averagesessions.data.sessions);
-
-    //console.log("JSON.parse", JSON.parse(averagesessions.data));
-
-
-    //console.log("Object.keys(jsonObj)", Object.keys(averagesessions.data));
-
-    //<Duration  props={averagesessions.data.sessions}    />
+     
+    
+   
+async function getMainData() {
+    const response = await axios.get('http://localhost:3000/user/'+id)
+    return setMainData(response.data.data); 
+}
+useEffect(() => {
+getMainData()}, [mainData]);
 
 
 
+     
+ 
+async function getActivity() {
+    const response = await axios.get('http://localhost:3000/user/'+id+'/activity')
+    return setActivity(response.data.data); 
+}
+useEffect(() => {
+getActivity()}, []);
+
+//console.log("Activity", activity);
 
 
 
+
+
+
+
+
+async function getSessions() {
+    const response = await axios.get('http://localhost:3000/user/'+id+'/average-sessions')
+    return setAvergagesessions(response.data.data.sessions);}
+useEffect(() => {
+getSessions()}, [averagesessions]);
+
+
+
+useEffect(() => {
+console.log("mainData", mainData);
+console.log("DONNNNEEEEEESS",averagesessions);
+console.log("Activity", activity);},[]);
+
+if(mainData.length===0){return<p>Chargement des data...</p>;}
     return (
         <div>
-            <Duration weeksessions={averagesessions.data.sessions} />
+          Bonjour {mainData.userInfos.firstName}
+          <div className="charts-container">
+           <ActivityChart />
+           <AverageSessionsChart averagesessions={averagesessions} /> 
+
+            {/* <AverageSessionsChart /> */}
+
+            {/* <LinearChart /> */}
+            </div>  
         </div>
     );
 };
